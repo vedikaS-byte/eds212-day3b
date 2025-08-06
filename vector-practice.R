e908546e17e4d253f1bbf139fc3bd6218a00fe0d
vedikaS-byte <vedika.shirtekar@gmail.com>
2025-08-06 13:58:13 -0700

    vector practice

--- vector-practice.R
+++ vector-practice.R
@@ -1,10 +1,20 @@
+## create and store vector
+marmots <- c("blue", "green", 4, "yellow")
 
+## create and store numeric vector
+pika <- c(12.4, 6.8, 3.9, 8.5)
+bear <- c(20L, 3L, 5L, 18L, 23L) # integers
 
+ringtail <- c(4.3, 8.9, 2.6, 7.1)
+fox <- c(9.0, 12.5, 5.4, 10.9)
 
+ringtail + fox
+ringtail-fox
+ringtail*100 #scalar 
+ringtail %*% fox # dot product
 
-
-
-
-
+a<- c(1,6)
+b <- c(2,4)
+c<- a+b
 
 
